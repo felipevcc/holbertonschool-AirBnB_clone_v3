f71ba02ddae392f3b74e995019a42f2e9c82d0db
felipevcc <felipevillamizarc@gmail.com>
2023-04-14 19:20:42 -0500

    app.py file documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,13 @@ from api.v1.views import app_views
 from os import getenv
 
 
+""" Start Flask """
 app = Flask(__name__)
+
+""" Register the blueprint app_views """
 app.register_blueprint(app_views)
-API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-API_PORT = getenv("HBNB_API_PORT", 5000)
+
+"""Create the CORS instance to allow IPs"""
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
@@ -27,4 +30,6 @@ def errorhandler(error):
 
 
 if __name__ == "__main__":
+    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    API_PORT = getenv("HBNB_API_PORT", 5000)
     app.run(host=API_HOST, port=API_PORT, threaded=True)
